bfa3932caff97bd978315558f1e3cf6b98cde9ac
SpiralP <spiral.palmer@gmail.com>
2019-03-27 16:50:42 -0700

    working png/jpeg from clipboard image

--- src/clip_image.rs
+++ src/clip_image.rs
@@ -1,10 +1,11 @@
 use clip_sys::*;
+use image::ColorType;
 
 pub struct ClipImage {
   ptr: CClipImage,
 }
 
-impl ClipImage {
+impl<'a> ClipImage {
   pub fn from_ptr(ptr: CClipImage) -> Self {
     Self { ptr }
   }
@@ -13,10 +14,10 @@ impl ClipImage {
     unsafe { clip_get_image_spec(self.ptr) }
   }
 
-  pub fn get_data(&self) {
-    let _data = unsafe { clip_get_image_data(self.ptr) };
-
-    unimplemented!()
+  pub fn get_data(&self) -> &'a [u8] {
+    let spec = self.get_spec();
+    let len: usize = spec.bytes_per_row as usize * spec.height as usize;
+    unsafe { std::slice::from_raw_parts(clip_get_image_data(self.ptr) as *const u8, len) }
   }
 }
 
@@ -26,4 +27,46 @@ impl Drop for ClipImage {
       clip_delete_image(self.ptr);
     }
   }
+}
+
+#[test]
+fn test_png() {
+  use super::Clip;
+
+  let mut output_writer = std::fs::File::create("test.png").unwrap();
+  let encoder = image::png::PNGEncoder::new(&mut output_writer);
+
+  let clip_image = Clip::get_image().unwrap();
+  let clip_spec = clip_image.get_spec();
+  let clip_data = clip_image.get_data();
+
+  encoder
+    .encode(
+      clip_data,
+      clip_spec.width as u32,
+      clip_spec.height as u32,
+      ColorType::RGBA(8),
+    )
+    .unwrap();
+}
+
+#[test]
+fn test_jpeg() {
+  use super::Clip;
+
+  let mut output_writer = std::fs::File::create("test.jpeg").unwrap();
+  let mut encoder = image::jpeg::JPEGEncoder::new(&mut output_writer);
+
+  let clip_image = Clip::get_image().unwrap();
+  let clip_spec = clip_image.get_spec();
+  let clip_data = clip_image.get_data();
+
+  encoder
+    .encode(
+      clip_data,
+      clip_spec.width as u32,
+      clip_spec.height as u32,
+      ColorType::RGBA(8),
+    )
+    .unwrap();
 }
