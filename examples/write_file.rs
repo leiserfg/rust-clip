82ab7d9308a2976ad34f416a8a6273b0cd04784a
SpiralP <spiral.palmer@gmail.com>
2019-03-27 17:16:40 -0700

    add write_as_png/jpeg, example that write to a file

---
+++ examples/write_file.rs
@@ -0,0 +1,30 @@
+use clip::*;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+  match Clip::get_format() {
+    ClipFormat::Empty => {
+      println!("clipboard is empty!");
+    }
+
+    ClipFormat::Text => {
+      {
+        let mut file = File::create("test.txt").unwrap();
+        file
+          .write_all(Clip::get_text().unwrap().as_bytes())
+          .unwrap();
+      }
+      println!("wrote to test.txt");
+    }
+
+    ClipFormat::Image => {
+      {
+        let image = Clip::get_image().unwrap();
+        image.write_as_png(&mut File::create("test.png").unwrap());
+      }
+
+      println!("wrote to test.png");
+    }
+  }
+}
--- src/clip_image.rs
+++ src/clip_image.rs
@@ -1,5 +1,6 @@
 use clip_sys::*;
 use image::ColorType;
+use std::io::Write;
 
 pub struct ClipImage {
   ptr: CClipImage,
@@ -14,10 +15,42 @@ impl<'a> ClipImage {
     unsafe { clip_get_image_spec(self.ptr) }
   }
 
-  pub fn get_data(&self) -> &'a [u8] {
+  pub fn get_data(&'a self) -> &'a [u8] {
     let spec = self.get_spec();
     let len: usize = spec.bytes_per_row as usize * spec.height as usize;
     unsafe { std::slice::from_raw_parts(clip_get_image_data(self.ptr) as *const u8, len) }
+  }
+
+  pub fn write_as_png<W: Write>(&self, writer: &mut W) {
+    let encoder = image::png::PNGEncoder::new(writer);
+
+    let clip_spec = self.get_spec();
+    let clip_data = self.get_data();
+
+    encoder
+      .encode(
+        clip_data,
+        clip_spec.width as u32,
+        clip_spec.height as u32,
+        ColorType::RGBA(8), // TODO check spec.bits_per_pixel
+      )
+      .unwrap();
+  }
+
+  pub fn write_as_jpeg<W: Write>(&self, writer: &mut W) {
+    let mut encoder = image::jpeg::JPEGEncoder::new(writer);
+
+    let clip_spec = self.get_spec();
+    let clip_data = self.get_data();
+
+    encoder
+      .encode(
+        clip_data,
+        clip_spec.width as u32,
+        clip_spec.height as u32,
+        ColorType::RGBA(8), // TODO check spec.bits_per_pixel
+      )
+      .unwrap();
   }
 }
 
@@ -30,43 +63,19 @@ impl Drop for ClipImage {
 }
 
 #[test]
-fn test_png() {
+fn test_write_image_as_png() {
   use super::Clip;
+  use std::fs::File;
 
-  let mut output_writer = std::fs::File::create("test.png").unwrap();
-  let encoder = image::png::PNGEncoder::new(&mut output_writer);
-
-  let clip_image = Clip::get_image().unwrap();
-  let clip_spec = clip_image.get_spec();
-  let clip_data = clip_image.get_data();
-
-  encoder
-    .encode(
-      clip_data,
-      clip_spec.width as u32,
-      clip_spec.height as u32,
-      ColorType::RGBA(8),
-    )
-    .unwrap();
+  let image = Clip::get_image().unwrap();
+  image.write_as_png(&mut File::create("test.png").unwrap());
 }
 
 #[test]
-fn test_jpeg() {
+fn test_write_image_as_jpeg() {
   use super::Clip;
+  use std::fs::File;
 
-  let mut output_writer = std::fs::File::create("test.jpeg").unwrap();
-  let mut encoder = image::jpeg::JPEGEncoder::new(&mut output_writer);
-
-  let clip_image = Clip::get_image().unwrap();
-  let clip_spec = clip_image.get_spec();
-  let clip_data = clip_image.get_data();
-
-  encoder
-    .encode(
-      clip_data,
-      clip_spec.width as u32,
-      clip_spec.height as u32,
-      ColorType::RGBA(8),
-    )
-    .unwrap();
+  let image = Clip::get_image().unwrap();
+  image.write_as_jpeg(&mut File::create("test.jpeg").unwrap());
 }
