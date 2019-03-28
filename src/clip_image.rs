23e795a8aa835160e7840d5be70deaed9f4e8504
SpiralP <spiral.palmer@gmail.com>
2019-03-28 10:59:31 -0700

    encoder trait

--- src/clip_image.rs
+++ src/clip_image.rs
@@ -1,9 +1,41 @@
 use clip_sys::*;
+use image::bmp::BMPEncoder;
+use image::jpeg::JPEGEncoder;
+use image::png::PNGEncoder;
 use image::ColorType;
+use std::io;
 use std::io::Write;
 
 pub struct ClipImage {
   ptr: CClipImage,
+}
+
+pub trait Encoder {
+  fn encode(self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()>;
+}
+
+impl<W: Write> Encoder for PNGEncoder<W> {
+  fn encode(self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
+    PNGEncoder::encode(self, data, width, height, color)
+  }
+}
+
+impl<'a, W: 'a> Encoder for JPEGEncoder<'a, W>
+where
+  W: Write,
+{
+  fn encode(mut self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
+    JPEGEncoder::encode(&mut self, data, width, height, color)
+  }
+}
+
+impl<'a, W: 'a> Encoder for BMPEncoder<'a, W>
+where
+  W: Write,
+{
+  fn encode(mut self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
+    BMPEncoder::encode(&mut self, data, width, height, color)
+  }
 }
 
 impl<'a> ClipImage {
@@ -22,26 +54,34 @@ impl<'a> ClipImage {
   }
 
   pub fn write_as_png<W: Write>(&self, writer: &mut W) {
-    let encoder = image::png::PNGEncoder::new(writer);
-
-    let clip_spec = self.get_spec();
-    let clip_data = self.get_data();
-
-    encoder
-      .encode(
-        clip_data,
-        clip_spec.width as u32,
-        clip_spec.height as u32,
-        ColorType::RGBA(8), // TODO check spec.bits_per_pixel
-      )
-      .unwrap();
+    self.write_from_encoder(PNGEncoder::new(writer));
   }
 
   pub fn write_as_jpeg<W: Write>(&self, writer: &mut W) {
-    let mut encoder = image::jpeg::JPEGEncoder::new(writer);
+    self.write_from_encoder(JPEGEncoder::new(writer));
+  }
 
+  pub fn write_as_bmp<W: Write>(&self, writer: &mut W) {
+    self.write_from_encoder(BMPEncoder::new(writer));
+  }
+
+  pub fn write_from_encoder<E: Encoder>(&self, encoder: E) {
     let clip_spec = self.get_spec();
     let clip_data = self.get_data();
+
+    println!("{:#?}", clip_spec);
+
+    assert!(clip_spec.bits_per_pixel == 32);
+
+    assert!(clip_spec.red_shift == 0);
+    assert!(clip_spec.green_shift == 8);
+    assert!(clip_spec.blue_shift == 16);
+    assert!(clip_spec.alpha_shift == 24);
+
+    assert!(clip_spec.red_mask == 0b1111_1111);
+    assert!(clip_spec.green_mask == 0b1111_1111 << 8);
+    assert!(clip_spec.blue_mask == 0b1111_1111 << 16);
+    assert!(clip_spec.alpha_mask == 0b1111_1111 << 24);
 
     encoder
       .encode(
